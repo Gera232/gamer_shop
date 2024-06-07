2496b78e2aee330c661122fa926a9791d0d4d033
Gera N. <Geranava232@gmail.com>
2024-06-07 19:19:28 -0300

    1.0

--- main.go
+++ main.go
@@ -5,14 +5,11 @@ import (
 	"backend/storage"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	time.Sleep(time.Second * 5)
-
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
