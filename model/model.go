47c8b54017aa534ebdf183d18f957fd32a2718c2
Gera N. <Geranava232@gmail.com>
2024-06-20 02:48:45 -0300

    .

---
+++ model/model.go
@@ -0,0 +1,36 @@
+package model
+
+type place string
+type role string
+
+const (
+	work   place = "work"
+	home   place = "house"
+	user   role  = "user"
+	admin  role  = "admin"
+	worker role  = "worker"
+)
+
+type Address struct {
+	ID             uint32 `json:"id"`
+	Place          place  `json:"place"`
+	Street         string `json:"street"`
+	Height         string `json:"height"`
+	Floor          uint16 `json:"floor"`
+	Department     uint16 `json:"department"`
+	BetweenStreets string `json:"betweenStreets"`
+	Observations   string `json:"observations"`
+	ProvinceID     uint32 `json:"provinceID"`
+}
+
+type Account struct {
+	ID        uint32 `json:"id"`
+	Name      string `json:"name"`
+	Surname   string `json:"surname"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	Role      role   `json:"role"`
+	AddressID uint32 `json:"addressID"`
+	CardID    uint32 `json:"cardID"`
+}
+type Accounts []*Account
