6c1e8d58a41ed7b8609b0aad7674345f26759ed9
Gera N. <Geranava232@gmail.com>
2024-07-10 10:29:45 -0300

    .

--- types/types.go
+++ types/types.go
@@ -4,11 +4,14 @@ type place string
 type role string
 
 const (
-	user   role  = "user"
-	admin  role  = "admin"
-	worker role  = "worker"
-	work   place = "work"
-	home   place = "house"
+	// Role
+	user   role = "user"
+	admin  role = "admin"
+	worker role = "worker"
+
+	// Place
+	work place = "work"
+	home place = "house"
 )
 
 type Address struct {
@@ -25,12 +28,11 @@ type Address struct {
 }
 
 type Account struct {
-	ID         uint32 `json:"id"`
-	Name       string `json:"name"`
-	Surname    string `json:"surname"`
-	Email      string `json:"email"`
-	Password   string `json:"password"`
-	Role       role   `json:"role"`
-	Address_id uint32 `json:"address_id"`
+	ID       uint32 `json:"id"`
+	Name     string `json:"name"`
+	Surname  string `json:"surname"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Role     role   `json:"role"`
 }
 type Accounts []*Account
