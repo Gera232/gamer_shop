e4584aeef1702ee400bf0960f683d6bb692bc3ab
Gera N. <Geranava232@gmail.com>
2024-07-30 14:40:47 -0300

    .

--- main.go
+++ main.go
@@ -15,15 +15,26 @@ func main() {
 		log.Fatal(err)
 	}
 
-	file, err := os.Open("./sql/01-db.sql")
+	file1, err := os.Open("./sql/01-db.sql")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer file.Close()
+	defer file1.Close()
+
+	file2, err := os.Open("./sql/03-insert.sql")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file2.Close()
 
 	storage.NewDB()
 
-	err = storage.Migrate(file)
+	err = storage.Migrate(file1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = storage.Migrate2(file2)
 	if err != nil {
 		log.Fatal(err)
 	}
--- storage/migrate.go
+++ storage/migrate.go
@@ -26,3 +26,23 @@ func Migrate(file *os.File) error {
 
 	return nil
 }
+
+func Migrate2(file *os.File) error {
+	sentence, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sentences := strings.Split(string(sentence), ";")
+
+	for _, sentence := range sentences {
+		if strings.TrimSpace(sentence) != "" {
+			_, err := db.Exec(sentence)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	return nil
+}
